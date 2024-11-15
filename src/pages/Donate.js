f908f8c21d69dd97b54137e6a34f2a12ba296c2d
mercie-ux <mbaomercy0@gmail.com>
2024-11-15 08:07:09 +0300

    Initial commit

---
+++ src/pages/Donate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Donate() {
+  return (
+    <div>
+      <h2>Donate with Bitcoin or Lightning</h2>
+      <p>Select your payment method to help developers fund their projects.</p>
+      <a href="https://your-btcpay-server-url" target="_blank" rel="noopener noreferrer">
+        Donate with BTCPay Server
+      </a>
+    </div>
+  );
+}
+
+export default Donate;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+    return (
+      <div>
+        <h1>Impact Donate</h1>
+        <p>Bitcoin to Help Raise Funds for Developers to Fund Their Projects.</p>
+        <a href="/donate">Donate Now</a>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
